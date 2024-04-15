2a09f80642a090ea1608d306d6503066b366e41b
victorinais <93883725+victorinais@users.noreply.github.com>
2024-04-15 17:54:40 -0500

    Add files via upload

---
+++ Data/baseContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using LaborSystem.Models;
+namespace LaborSystem.Data;
+
+public class BaseContext : DbContext{
+    public BaseContext(DbContextOptions<BaseContext> options) : base(options){
+        
+    }
+}
