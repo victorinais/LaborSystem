f4eec3a095fbf44ab4833ba1d956b976c6a47354
victorinais <vicmarin50@gmail.com>
2024-04-18 18:20:34 -0500

    cambios del profesor

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,6 +1,11 @@
-using LaborSystem.Data;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using LaborSystem.Models;
+using ViewModels.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using LaborSystem.Data;
+
 
 namespace LaborSystem.Controllers;
 public class EmployeesController : Controller {
@@ -8,9 +13,21 @@ public class EmployeesController : Controller {
     public EmployeesController(BaseContext context){
         _context = context;
     }
+    //Listar todos los Empleados
     public async Task<IActionResult> Index(){
         return View(await _context.Employees.ToListAsync());
     }
+    //Detalles de empleados por Id
+    public async Task<IActionResult> Details(int? id)
+    {
+        var employee = await _context.Employees.FirstOrDefaultAsync(e => e.Id == id);
+        var viewModel = new RegistrationViewModel
+        {
+            Employee = employee,
+        };
+        return View(viewModel);
+    }
+
 
 }
 
